14e75ab121d9735b8089c232059db9965d02ff0a
lucasjdelri0 <lucasjdelrio@gmail.com>
2022-08-12 09:27:32 +0200

    Apply survey component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "antd";
+import { BigNumber, ethers } from "ethers";
 import Page from "components/layout/Page";
+import Survey from "components/Survey";
 import { useMetaMaskAccount } from "providers/MetaMaskProvider";
-import logo from "./logo.svg";
+import { getQuizContract } from "utils/contractHelpers";
+import surveySamples from "utils/survey-sample.json";
 import "./App.css";
+import { Survey as TSurvey } from "components/Survey/Survey.props";
 
 const App = () => {
-  const { connectedAccount } = useMetaMaskAccount();
+  const [surveyStarted, setSurveyStarted] = useState(false);
+  const { connectedAccount, provider } = useMetaMaskAccount();
+  const dailySurvey: TSurvey =
+    surveySamples[Math.floor(Math.random() * surveySamples.length)];
+
+  useEffect(() => {
+    const getContractInfo = async () => {
+      const quizContract = getQuizContract(provider);
+      const symbol = await quizContract.symbol();
+      const balanceBN = await quizContract.balanceOf(connectedAccount);
+      const balance = ethers.utils.formatEther(balanceBN);
+      console.log("balance", balance);
+    };
+
+    if (provider && connectedAccount) {
+      getContractInfo();
+    }
+  }, [connectedAccount, provider]);
 
   return (
     <Page>
@@ -18,9 +39,20 @@ const App = () => {
         </div>
       ) : (
         <div className="App-content">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p style={{ color: "black" }}>Survey Challenge by Lucas Del Rio</p>
-          <Button type="primary">Start</Button>
+          {!surveyStarted ? (
+            <>
+              <img src={dailySurvey.image} className="App-logo" alt="logo" />
+              <p style={{ color: "black" }}>{dailySurvey.title}</p>
+              <Button
+                type="primary"
+                onClick={() => setSurveyStarted(!surveyStarted)}
+              >
+                Start Survey
+              </Button>
+            </>
+          ) : (
+            <Survey dataSource={dailySurvey} />
+          )}
         </div>
       )}
     </Page>
