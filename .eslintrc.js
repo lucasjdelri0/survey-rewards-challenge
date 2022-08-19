34d9ee4c083ecf788bdd50acd4ecb04459b6fe0c
lucasjdelri0 <lucasjdelrio@gmail.com>
2022-08-19 06:54:24 +0200

    Configure eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript',
+    'plugin:react/jsx-runtime',
+    'eslint-config-prettier',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['tsconfig.json'],
+  },
+  plugins: ['react'],
+  rules: {
+    'no-unused-vars': 'warn',
+    quotes: 'off',
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/no-floating-promises': 'warn',
+    '@typescript-eslint/no-misused-promises': [
+      'error',
+      { checksVoidReturn: false },
+    ],
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+          'builtin',
+        ],
+      },
+    ],
+  },
+}
