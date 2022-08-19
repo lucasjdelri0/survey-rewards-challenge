15e4041d220e283e1e5a201e47226b59c11ac3ba
lucasjdelri0 <lucasjdelrio@gmail.com>
2022-08-19 07:23:26 +0200

    Include parser and ignore lint in unnecessary files

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     es2021: true,
     jest: true,
   },
+  parser: '@typescript-eslint/parser',
   settings: {
     react: {
       version: 'detect',
--- src/components/layout/Header/Header.props.ts
+++ src/components/layout/Header/Header.props.ts
@@ -1,11 +1,11 @@
-export type Route = {
+export interface Route {
   path: string
   title: string
 }
 
 export type MyRoutes = Route[]
 
-export type HeaderProps = {
+export interface HeaderProps {
   backgroundColor?: string
   repoHref?: string
   avatarImageSrc?: string
